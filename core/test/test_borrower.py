29304ec2804594e01398d125e17ee4e0ed4d3ded
riansyah <mriansyah93@gmail.com>
2021-12-19 21:32:20 +0800

    init test for borrower

---
+++ core/test/test_borrower.py
@@ -0,0 +1,15 @@
+# test_borrower.py
+
+# from django.contrib.admin.sites import AdminSite
+# from django.contrib.auth.models import User
+from django.test import TestCase
+
+# from core.admin import CompanyAdmin
+# from core.models import Company
+
+class BorrowerTest(object):
+	"""docstring for BorrowerTest"""
+	def __init__(self, arg):
+		super(BorrowerTest, self).__init__()
+		self.arg = arg
+		
